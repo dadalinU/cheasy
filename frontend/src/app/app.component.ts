c65d35fa2ba0a55e04df9625dc6c6d9029d1858b
Michael Ebneth <michael.ebneth@gmx.de>
2023-01-15 10:42:20 +0100

    chore: :truck: change folder structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cheasy';
+  test = {
+    param1: 'test',
+    param2: 'test2',
+    param3: 'test3',
+    param4: 'test',
+    param5: 'test',
+  };
 }
