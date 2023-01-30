ce1f0e892ea070f9e5eab05cf5053d38ac0e4ff3
Linda <lindaxyzz4@gmail.com>
2023-01-30 21:01:50 +0100

    Add Components, Layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeDetailComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    HeaderComponent,
+    FilterComponent,
+    MenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
